4723dc3dfed1eef79fdd209c3fa47018c1c723d6
gizmore <gizmore@wechall.net>
2017-07-20 17:09:51 +0200

    sync

--- tpl/listitem/board.php
+++ tpl/listitem/board.php
@@ -8,7 +8,11 @@ $readClass = $board->hasUnreadPosts($user) ? 'gwf-forum-unread' : 'gwf-forum-rea
     <h4><?= $board->displayDescription(); ?></h4>
     <p><?= t('board_stats', [$board->getThreadCount(), $board->getPostCount()]); ?></p>
   </div>
-  
+
+<?php if (GWF_UserSetting::get('forum_subscription')->getValue() !== GDO_ForumSubscribe::ALL) : ?>
+  <?= GDO_IconButton::make()->href(href('Forum', 'Subscribe', '&board='.$board->getID()))->icon('block'); ?>
+
+<?php endif; ?>
   <?= GDO_Icon::iconS('arrow_right'); ?>
       
 </md-list-item>
